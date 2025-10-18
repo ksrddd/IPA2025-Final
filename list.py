bc57934bd60daba55041e1c985cf993651fe664e
sukhumrudee <66070315@kmitl.ac.th>
2025-10-18 21:42:46 +0700

    add : check list bearer token and token_room_id

---
+++ list.py
@@ -0,0 +1,17 @@
+import requests
+import json
+
+access_token = "My Access Token"
+url = 'https://webexapis.com/v1/rooms'
+
+headers = {
+    'Authorization': 'Bearer {}'.format(access_token),
+    'Content-Type': 'application/json'
+}
+
+params = {
+    'max': 100
+}
+
+res = requests.get(url, headers=headers, params=params)
+print(json.dumps(res.json(), indent=4))
